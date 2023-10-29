eb490df690e3c6c4f52d772abe5b558bbe9db8a1
ElaaBoulifi <elaa.boulifi@esprit.tn>
2023-10-29 20:53:29 +0100

    commentaire

--- src/main/java/tn/esprit/ReclamationModule/Controller/ArticleController.java
+++ src/main/java/tn/esprit/ReclamationModule/Controller/ArticleController.java
@@ -15,6 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/articles")
 public class ArticleController {
     private static final Logger logger = LoggerFactory.getLogger(ReclamationController.class);
